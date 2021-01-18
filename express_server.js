247295a7219f6a23f10444c648ecd4ceb78e456e
Your Name <thomas.w.yee@gmail.com>
2021-01-18 14:20:06 -0500

    add urls.json route

--- express_server.js
+++ express_server.js
@@ -11,6 +11,10 @@ app.get('/', (req, res) => {
   res.send('Hello!');
 });
 
+app.get('/urls.json', (req, res) => {
+  res.status(200).json(urlDatabase);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
